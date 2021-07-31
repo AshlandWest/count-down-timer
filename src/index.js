804f039f42f474cb0a6178479cea9d734757369f
Ash <ashlandmwest@gmail.com>
2021-07-31 13:43:42 -0700

    Remove redux dependency

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <React.StrictMode>
